882a5d42807f166604538d933fe60516ea12cd83
Tobe <teokafor@gmail.com>
2023-03-07 15:07:00 -0600

    implemented image carousels

--- main.js
+++ main.js
@@ -22,3 +22,39 @@ function accordion(contentId, carrotId) {
         targetCarrot.className = targetCarrot.className.replace(" project-tab-carrot-down", "");
     }
 }
+
+
+function imageCarousel(x, dir) {
+    let parentElement = document.getElementById(x);
+    let adjImages = parentElement.getElementsByClassName('carousel-image');
+
+    for (let i = 0; i < adjImages.length; i++) {
+        console.log(adjImages[i].className.indexOf("image-current"));
+        if (adjImages[i].className.indexOf("image-current") == 15) { // Hide the current image
+            adjImages[i].className = adjImages[i].className.replace("image-current", "image-hidden");
+
+            if (dir=='prev') {
+                if (i == 0) { // Wrap images from first to last
+                    console.log("wrapping to end...");
+                    adjImages[adjImages.length-1].className = adjImages[adjImages.length-1].className.replace("image-hidden", "image-current");
+                    break;
+                } else { // Otherwise, show prev image in array
+                    console.log("show prev image...");
+                    adjImages[i-1].className = adjImages[i-1].className.replace("image-hidden", "image-current");
+                    break;
+                }
+            } else if (dir == 'next') {
+                if (i == adjImages.length-1) {
+                    console.log("wrapping to start...");
+                    adjImages[0].className = adjImages[0].className.replace("image-hidden", "image-current");
+                    break;
+                } else {
+                    console.log("showing next image...");
+                    adjImages[i+1].className = adjImages[i+1].className.replace("image-hidden", "image-current");
+                    break;
+                }
+            }
+        }
+    }
+    console.log('break success')
+}
